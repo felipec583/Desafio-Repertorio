2b69de6359f992a9fb52b0dd62d143ac71924b1a
Fabian Leiva <123709798+fabianleiva@users.noreply.github.com>
2024-02-24 20:11:38 -0300

    add views router, edit prefix for song routes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
 import express from "express";
 import cors from "cors";
+import viewsRoutes from "./routes/viewsRoutes.js";
 import songRoutes from "./routes/songRoutes.js";
-const app = express();
-import path from "path";
+
 const PORT = 3000;
+const app = express();
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
   console.log(`Method ${req.method}`);
   next();
 });
-app.use("/canciones", songRoutes);
-
-app.get("/", async (req, res) => {
-  res.status(200).sendFile(path.resolve("index.html"));
-});
+app.use("/", viewsRoutes);
+app.use("/api/v1", songRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT${PORT}`);
